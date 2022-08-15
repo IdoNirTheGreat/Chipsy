54be09c04bf4f6f7675c597f840980f51fd03e28
IdoNirTheGreat <Idonir777@gmail.com>
2022-08-15 12:16:06 +0300

    Small Changes: -Added error codes. - Filename is given via system arguments.

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 # include "chip8.h"
 # include <stdio.h>
 
-
-int main()
+enum error_code
 {
+    EXIT_SUCCESSFULLY,
+    TOO_MANY_ARGUMENTS
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf_s("Too many arguments!\n");
+        return TOO_MANY_ARGUMENTS;
+    }
+    
     CHIP8 chipsy = {};
-    init_chip8(chipsy, "");
+    init_chip8(chipsy, argv[1]);
 }
