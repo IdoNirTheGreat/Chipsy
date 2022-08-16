09884f3c7505c56f3dcb9387402d348080e46ea8
IdoNirTheGreat <Idonir777@gmail.com>
2022-08-16 23:44:59 +0300

    Main File Updated: - A for loop is now written so the first N (currently 50) cycles are-  monitored.

--- main.c
+++ main.c
@@ -4,17 +4,23 @@
 enum error_code
 {
     EXIT_SUCCESSFULLY,
-    TOO_MANY_ARGUMENTS
+    INCORRECT_AMOUNT_ARGUMENTS
 };
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf_s("Too many arguments!\n");
-        return TOO_MANY_ARGUMENTS;
+        printf_s("Incorrect amount of arguments!\n");
+        return INCORRECT_AMOUNT_ARGUMENTS;
     }
     
     CHIP8 chipsy = {};
-    init_chip8(chipsy, argv[1]);
+    init_chip8(&chipsy, argv[1]);
+    for (int i = 0; i < 50; ++i)
+    {
+        printf_s("Cycle %d: ", i+1);
+        cycle(&chipsy);
+        printf_s("\n");
+    }
 }
