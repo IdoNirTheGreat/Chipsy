08d56395637988c68d59dfc14fd56ebbaa79d714
IdoNirTheGreat <Idonir777@gmail.com>
2022-08-20 18:07:03 +0300

    Wrote function to update SDL renderer, not tested!

--- main.c
+++ main.c
@@ -12,8 +12,24 @@ enum error_code
     GUI_FAILED,
 };
 
+void update_screen(SDL_Renderer* renderer, unsigned char* monitor)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    for (unsigned int row = 0; row < ROWS; ++row)
+    {
+        for (unsigned int col = 0; col < COLS; ++col)
+        {
+            SDL_RenderDrawPoint(renderer, row, col);
+        }
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
 int WinMain(int argc, char* args[])
 {
+    // SDL Setup:
+    /*
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
 
@@ -24,34 +40,57 @@ int WinMain(int argc, char* args[])
     }
 
     window = SDL_CreateWindow(
-			    "Chipsy - A CHIP-8 Emulator in C!",
-			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			    SCREEN_WIDTH, SCREEN_HEIGHT,
-			    SDL_WINDOW_SHOWN
-			    );
+	 		    "Chipsy - A CHIP-8 Emulator in C!",
+	 		    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+	 		    SCREEN_WIDTH, SCREEN_HEIGHT,
+	 		    SDL_WINDOW_SHOWN
+	 		    );
 
     if (window == NULL)
     {
         fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
         exit(GUI_FAILED);
     }
-    
+
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 0));
+    SDL_UpdateWindowSurface(window);
+    */
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+    SDL_RenderSetScale(renderer, 1, 1);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // SDL_RenderDrawPoint(renderer, 0, 0);
+
+    SDL_RenderPresent(renderer);
+
+    // CHIP-8 Setup:
     CHIP8 chipsy = {};
     init_chip8(&chipsy, FILENAME);
-    for (int i = 0; i < 50; ++i)
+
+    // Emulated CPU Cycles:
+    for (int i = 0; i < 132; ++i)
     {
         // printf_s("Value at 0x%x = 0x%x\n", START_ADDRESS+i, chipsy.memory[START_ADDRESS+i]);
         printf_s("Cycle %d: \n", i+1);
         cycle(&chipsy);
+        update_screen(renderer, chipsy.monitor);
+        SDL_Delay(500);
         printf_s("\n");
     }
 
-    screenSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 255, 0));
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(2000);
+    SDL_Delay(5000);
+    // Program Termination:
     SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
     SDL_Quit();
     printf_s("Program exited successfully.\n");
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
