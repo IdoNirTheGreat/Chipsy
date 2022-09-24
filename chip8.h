8697df86a6ce8be2d6ade550b0f414a2372cf0ce
IdoNirTheGreat <Idonir777@gmail.com>
2022-09-25 00:56:56 +0300

    Added comments.

--- chip8.h
+++ chip8.h
@@ -4,31 +4,100 @@
 # define ROWS 32
 # define KEYS 16
 
-extern const unsigned int START_ADDRESS; /** Start address for CHIP8's ROM.*/;
+extern const unsigned int START_ADDRESS;    /* 
+                                             * Start address 
+                                             * for the 
+                                             * CHIP8's ROM.
+                                             */
 
-typedef struct CHIP8 /** The CHIP8 Struct which holds all VM's attributes.*/
+typedef struct CHIP8    /* 
+                         * The struct which holds all of the 
+                         * CHIP-8's machines attributes.
+                         */
 {
-    unsigned char registers[16];
-    unsigned char memory[4096];
-    unsigned short index;
-    unsigned short pc;
-    unsigned short stack[16];
-    unsigned char sp;
-    unsigned char delay_timer;
-    unsigned char sound_timer;
-    unsigned char monitor[ROWS][COLS];
-    unsigned long opcode;
-    unsigned char update_screen;
-    unsigned char active_keys[KEYS];
-    unsigned char is_kbhit;
+    unsigned char registers[16];    /* 
+                                     * 16 registers with a 
+                                     * size of one byte each.
+                                     */
+
+    unsigned char memory[4096];     /* 
+                                     * ROM size of 4096 
+                                     * bytes.
+                                     */
+
+    unsigned short index;           /* The index pointer. */
+    unsigned short pc;              /* The program counter. */
+
+    unsigned short stack[16];       /* 
+                                     * A stack that holds up 
+                                     * to 16 instructions. 
+                                     */
+
+    unsigned char sp;               /* 
+                                     * A stack pointer 
+                                     * (points to the head 
+                                     * of the stack). 
+                                    */
+
+    unsigned char delay_timer;      /* 
+                                     * A delay timer with a 
+                                     * 60 [Hz] refresh rate. 
+                                    */
+
+    unsigned char sound_timer;      /* 
+                                    * A sound timer with a 60
+                                    * [Hz] refresh rate. 
+                                     */
+
+    unsigned char monitor[ROWS][COLS]; /* 
+                                        * A 2d array which 
+                                        * stores each 
+                                        * pixels's status. 
+                                        */
+
+    unsigned long opcode;   /* 
+                             * A byte that holds the current 
+                             * instruction's opcode.
+                             */
+
+    unsigned char update_screen;    /* 
+                                     * A flag that tells if 
+                                     * the screen needs to be
+                                     * refreshed.
+                                     */
+                                
+    unsigned char active_keys[KEYS]; /* 
+                                      * An array of flags 
+                                      * that tells if the key
+                                      * in the matching index
+                                      * is pressed or not.
+                                      */
+
+    unsigned char is_kbhit; /* 
+                             * A flag that tells if there is 
+                             * a key which is pressed down at
+                             * all.
+                             */
 } CHIP8; 
 
-void load_ROM(CHIP8* chip8, char const* filename); /** Loads ROM data from filename to chip8 VM.*/
+void load_ROM(CHIP8* chip8, char const* filename);  
+    /*
+     * Loads ROM data from filename to chip8 VM.
+     */
 
-void load_fonts(CHIP8* chip8); /** Loads fonts to chip8 VM.*/
+void load_fonts(CHIP8* chip8); 
+    /*
+     * Loads fonts to chip8 VM.
+     */
 
-void init_chip8(CHIP8* chip8, char const* rom); /** Main function to init VM.*/
+void init_chip8(CHIP8* chip8, char const* rom); 
+    /*
+     * Main function to init VM.
+     */
 
-void cycle(CHIP8* chip8); /** Operate one CPU cycle.*/
+void cycle(CHIP8* chip8);  
+    /*
+     * Operate one CPU cycle.
+     */
 
 #endif
